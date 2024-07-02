8f9827e9e757505399207195add4b2661be8a5fc
Anoop-x-codin <hothisanvir@yahoo.com>
2024-07-02 15:33:33 -0400

    Added padding to the service items individual boxes

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,7 +2,7 @@ export default function Nav() {
   return (
     <ul className="flex space-x-8">
       <li><a className="cursor-pointer">Home</a></li>
-      <li><a className="cursor-pointer">About Us</a></li>
+      <li><a className="cursor-pointer">AboutUs</a></li>
       <li><a className="cursor-pointer">Pricing</a></li>
       <li>
         <a className="cursor-pointer bg-blue-500 text-white px-6 py-2 rounded-full">
--- src/components/ServiceItem.js
+++ src/components/ServiceItem.js
@@ -2,7 +2,7 @@ export default function ServiceItem({ title, description, image }) {
   return (
     <div>
       {image}
-      <h4 className="text-xl font-bold">{title}</h4>
+      <h4 className="text-xl font-bold p-2">{title}</h4>
       <p style={{color:'#a3a3a3'}}>{description}</p>
     </div>
   );
