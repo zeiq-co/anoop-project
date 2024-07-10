488dd964f9cd56dc2937703e494c41c244de6b58
Anoop-x-codin <hothisanvir@yahoo.com>
2024-07-10 11:48:54 -0400

    added footer, made changes to heading and made contact infov thinner plus taller

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,4 +1,5 @@
 import Nav from '../components/Nav';
+import { Instagram, Twitter, Facebook, Youtube } from 'lucide-react';
 
 export default function Home() {
  return(
@@ -17,19 +18,40 @@ export default function Home() {
       {/* Header */} 
 
       {/* Title */} 
-      <h1 className="font-bold text-center p-8 text-2xl">Contact Us</h1>
-      <p className='font-extrabold text-center'>Any questions or remarks? Just write us a message!</p>
+      <h1 className="font-bold text-center p-8 text-5xl">Contact Us</h1>
+      <p className='font-extrabold text-center text-gray-700'>Any questions or remarks? Just write us a message!</p>
       {/* Title */}
 
       {/* Info */}
-        <div className= "bg-black m-3 rounded-lg text-white p-4 h-86 w-1/2">
+        <div className= "bg-black m-3 rounded-lg text-white p-4 h-86 w-1/3">
           <h1 className='font-bold text-2xl p-4'>Contact Information</h1>
-          <p className="text-gray-400 text-lg p-4">Say something to start a live chat!</p>
+          <p className='text-gray-400 text-lg p-4'>Say something to start a live chat!</p>
           <p className='p-4'>+1012 3456 789</p>
           <p className='p-4'>demo@gmail.com</p>
           <p className='p-4'>132 Dartmouth Street Boston, Massachusetts 02156 United States</p>
         </div>
       {/* Info */}
+
+      {/* Footer */}
+
+
+    <div className='flex flex-row justify-between bg-black p-8 h-60'>
+      <div className='space-y-3 text-white'>
+        <h4 className='text-white'>My Website</h4>
+        <Nav />
+        <p className='text-white'>© 2024 My Website. All rights reserved.</p>
+      </div>
+      <div className='flex flex-row space-x-3'>
+        <Instagram size={32} color='white' />
+        <Twitter size={32} color='white'/>
+        <Facebook size={32} color='white'/>
+        <Youtube size={32} color='white'/>
+      </div>
+    </div>
+
+
+      {/* Footer */}
+  
   </div>
  );
 }
