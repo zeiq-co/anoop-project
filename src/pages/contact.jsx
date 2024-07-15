4e4373660eb6b0bf8118d3d9af78dfe4dccb6670
Anoop-x-codin <hothisanvir@yahoo.com>
2024-07-15 11:18:36 -0400

    Made box thats next to black one
    It is not showing next to the black box even though i have used justify-end, instead its showing underneath.

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -4,7 +4,7 @@ import { Instagram, Twitter, Facebook, Youtube} from 'lucide-react';
 
 export default function Home() {
   return (
-    <div className='min-h-screen bg-white'>
+    <div className='min-h-screen bg-gray-50'>
       {/* Header */}
       <div className='flex flex-row bg-black p-3 text-white shadow-md'>
         <div className='flex flex-1'>
@@ -18,14 +18,15 @@ export default function Home() {
 
       {/* Title */}
       <h1 className='p-8 text-center text-5xl font-bold'>Contact Us</h1>
-      <p className='text-center font-extrabold text-gray-700'>
+      <p className='text-center font-extrabold text-gray-700 p-8'>
         Any questions or remarks? Just write us a message!
       </p>
       {/* Title */}
 
       {/* Info */}
       <div>
-        <div className='h-86 m-3 w-1/3 rounded-lg bg-black p-4 text-white'>
+        {/* Black Box */}
+        <div className='min-h-96 m-4 w-1/4 rounded-lg bg-black p-4 text-white'>
           <h1 className='p-4 text-2xl font-bold'>Contact Information</h1>
           <p className='p-4 text-lg text-gray-400'>Say something to start a live chat!</p>
           <div className='flex flex-row space-x-3 p-4'>
@@ -41,11 +42,18 @@ export default function Home() {
             <p> 132 Dartmouth Street Boston, Massachusetts 02156 United States </p>
           </div>
         </div>
+        {/* Black Box */}
+        {/* White Box */}
+        <div className='bg-white justify-self-end min-h-96 w-3/4'>
+
+
+        </div>
+        {/* White Box */}
       </div>
       {/* Info */}
 
       {/* Footer */}
-      <div className='flex h-60 flex-row justify-between bg-black p-8'>
+      <div className='flex flex-row justify-between bg-black p-8'>
         <div className='space-y-3 text-white'>
           <h4 className='text-white'>My Website</h4>
           <Nav />
