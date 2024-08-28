0359fca6e68386823f13c3887a70aa0423aa6b2f
Anoop-x-codin <hothisanvir@yahoo.com>
2024-08-28 16:53:57 -0600

    Update aboutus.js

--- src/pages/aboutus.js
+++ src/pages/aboutus.js
@@ -14,11 +14,16 @@ export default function Home () {
       {/* Title */}
       {/* Info Tab's */}
       <div className='flex flex-row-reverse'>
-      <div className='h-52 w-52  bg-yellow-200 rounded-full border-yellow-100 border-8 m-3 mr-10 shadow-lg'> 
+      <div className='h-52 w-52 mb- bg-yellow-200 rounded-full border-yellow-100 border-8 m-3 mr-10 shadow-lg'> 
         <p className='text-white text-center p-4 pt-16 text-3xl font-bold font-sans'> Owned By Anoop </p>
       </div>
-      <div className='h-52 w-52 bg-blue-200 rounded-full mr-80 border-blue-100 border-8 m-3 shadow-lg '> 
-        <p className='text-white text-center p-4 pt-16 text-3xl font-bold font-sans'> Owned By Anoop </p>
+      <div className='h-44 w-44 bg-blue-200 rounded-full mr-80 border-blue-100 border-8 m-3 shadow-lg '> 
+        <p className='text-white text-center p-4 pt-6 text-3xl font-bold font-sans'> Owned By Anoop </p>
+      </div>
+      </div>
+      <div className='flex flex-col'>
+      <div className='h-48 w-48 bg-purple-200 rounded-full  border-purple-100 border-8 shadow-lg flex flex-col mb-10 ml-10'> 
+        <p className='text-white text-center p-4 pt-16 text-3xl font-bold font-sans'> Owned by Anoop </p>
       </div>
       </div>
       {/* Info Tab's */}
